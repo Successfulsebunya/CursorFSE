a52b9330c3d42ca3503f9888c630c2d0a6286037
MoCursor <ssebunya.moses2@gmail.com>
2025-06-02 12:23:49 +0300

    Initial Commit
    Initial theme file Cursor FSE

---
+++ patterns/hidden-404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: 404
+ * Slug: cursorfse/404
+ * Inserter: no
+ *
+ * @package cursorfse
+ * @since 1.0.0
+ */
+?>
+<!-- wp:heading {"textAlign":"center", "level": 1} -->
+<h1 class="wp-block-heading has-text-align-center">
+	<?php esc_html_e( 'Page not found', 'cursorfse' ); ?>
+</h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'cursorfse' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'Search form label', 'cursorfse' ); ?>","showLabel":false,"buttonText":"<?php echo esc_html_x( 'Search', 'Search form submit button text', 'cursorfse' ); ?>"} /-->
